b5b45fc81686da70054d07a41f27dae2ee4e4329
hamie <elias.hami-ext@mfi.fr>
2022-04-13 14:03:42 +0200

    fix issues with bootstrap

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,7 +2,7 @@ import { NavLink } from '.'
 
 export default function Nav() {
   return (
-    <nav className="nvabr navbar-expand navbar-dask bg-dark">
+    <nav className="navbar navbar-expand navbar-dark bg-dark">
       <div className="navbar-nav">
         <NavLink href="/" exact className="nav-item nav-link">Home</NavLink>
         <NavLink href="/buildings" className="nav-item nav-link">Buildings</NavLink>
