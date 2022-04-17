39131e43d86b5d5ee1df114699a6c603ea20aa6f
hamie <elias.hami-ext@mfi.fr>
2022-04-17 13:16:40 +0200

    added buttons to addedit

--- components/constructions/AddEdit.tsx
+++ components/constructions/AddEdit.tsx
@@ -1,10 +1,10 @@
 import { useRouter } from 'next/router'
 import { SubmitHandler, useForm, UseFormProps } from 'react-hook-form'
-import { useState } from 'react'
 import { yupResolver } from '@hookform/resolvers/yup'
 
 import { constructionService, alertService } from 'services'
 import { Construction, constructionSchema } from 'types'
+import { Link } from 'components'
 
 type AddEditProps = {
   construction?: Construction
@@ -64,17 +64,33 @@ const AddEdit:React.FC<AddEditProps> = ({ construction }) => {
           <input type="text" {...register("name" as never)} className={'form-control' + (errors.name ? ' is-invalid' : '')}/>
           <div className="invaid-feedback">{errors.name?.message}</div>
         </div>
-        <div className="form-group col-5">
+        <div className="form-group col">
           <label>Address</label>
           <input type="text" {...register("address" as never)} className={'form-control' + (errors.address ? ' is-invalid' : '')}/>
           <div className="invaid-feedback">{errors.address?.message}</div>
         </div>
-        <div className="form-group col-2">
+        <div className="form-group col">
           <label>Type</label>
           <input type="text" {...register("type" as never)} className={'form-control' + (errors.type ? ' is-invalid' : '')}/>
           <div className="invaid-feedback">{errors.type?.message}</div>
         </div>
-      </div>
+        </div>
+        <div className="form-row">
+          <div className="form-group">
+            <button type="submit" disabled={formState.isSubmitting} className="btn btn-primary mr-2">
+              {formState.isSubmitted && <span className="spinner-border spinner-border-sm mr-1"></span>}
+              Save
+            </button>
+            <button
+              onClick={() => reset(formOptions.defaultValues)}
+              type="button"
+              disabled={formState.isSubmitting}
+              className="btn btn-secondary">
+              Reset
+            </button>
+            <Link href="/constructions" className="btn btn-link">Cancel</Link>
+          </div>
+        </div>
     </form>
   )
 }
