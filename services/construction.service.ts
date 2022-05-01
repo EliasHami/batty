e1ce059629a01719ba0be3d007bb763e2dc9039f
Elias <elias.ihma.gmail.com>
2022-05-01 15:15:46 +0200

    added api url

--- services/construction.service.ts
+++ services/construction.service.ts
@@ -1,24 +1,27 @@
 import { Constructions, Construction } from 'types'
+import { apiUrl } from 'config'
 import { fetchWrapper } from 'helpers' 
 
+const baseUrl = `${apiUrl}/constructions`
+
 const getAll = async():Promise<Constructions> => {
-  return fetchWrapper.get<Constructions>('/constructions')
+  return fetchWrapper.get<Constructions>(baseUrl)
 }
 
 const getById = async(id:number):Promise<Construction> => {
-  return fetchWrapper.get<Construction>(`/constructions/${id}`)
+  return fetchWrapper.get<Construction>(`${baseUrl}/${id}`)
 }
 
 const create = async(params:Construction):Promise<Construction> => {
-  return fetchWrapper.post<Construction>('/constructions', params)
+  return fetchWrapper.post<Construction>(baseUrl, params)
 }
 
 const update = async(id:number, params:Construction):Promise<Construction> => {
-  return fetchWrapper.put<Construction>(`/constructions/${id}`, params)
+  return fetchWrapper.put<Construction>(`${baseUrl}/${id}`, params)
 }
 
 const _delete = async(id:number):Promise<Construction> => {
-  return fetchWrapper.delete<Construction>(`/constructions/${id}`)
+  return fetchWrapper.delete<Construction>(`${baseUrl}/${id}`)
 }
 
 export const constructionService = {
