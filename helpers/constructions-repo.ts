7498f437eeda8fe55c90a4a1eb4169b994f03803
Elias <elias.ihma.gmail.com>
2022-05-01 18:47:32 +0200

    added repo create and update

--- helpers/constructions-repo.ts
+++ helpers/constructions-repo.ts
@@ -3,6 +3,10 @@ import { Construction, Constructions } from "types"
 
 const constructions : Constructions = require('data/constructions.json')
 
+const saveData = ():void => {
+  fs.writeFileSync('data/constructions.json', JSON.stringify(constructions, null, 4))
+}
+
 const getAll = ():Constructions => {
   return constructions as Constructions
 }
@@ -11,9 +15,49 @@ const getById = (id:number):Construction | null | undefined => {
   return constructions && constructions.find(c => c.id.toString() === id.toString())
 }
 
-const create = (construction:Construction):void => {}
+type CreateUpdateArgs = {
+  name: string,
+  address: string,
+  type: string
+}
 
-const update = (id:number, construction:Construction):void => {}
+const create = ({name, type, address}:CreateUpdateArgs):void => {
+  // validate
+  if (constructions?.find(c => c.name === name)) {
+    throw `Construction with name ${name} already exists`
+  }
+
+  // generate new construction id
+  const id = constructions?.length ? Math.max(...constructions.map(c => c.id)) + 1 : 1
+
+  // set date created and updated
+  const dateCreated = new Date().toISOString()
+  const dateUpdated = new Date().toISOString()
+
+  // add and save construction
+  const construction = {id, name, type, address, dateCreated, dateUpdated, isDeleting: false}
+  constructions?.push(construction as Construction)
+  saveData()
+}
+
+const update = (id:number, { name, type, address}:CreateUpdateArgs):void => {
+  const construction = constructions?.find(c => c.id.toString() === id.toString())
+  
+  if (!construction) {
+    throw `Construction with id ${id} does not exist`
+  }
+
+  if (name !== construction.name && constructions?.find(c => c.name === name)) {
+    throw `Construction with name ${name} already exists`
+  }
+
+  // set date updated
+  construction.dateUpdated = new Date().toISOString()
+
+  // update construction and save
+  Object.assign(construction, {name, type, address})
+  saveData()
+}
 
 const _delete = (id:number):void => {}
 
--- types/constructions.ts
+++ types/constructions.ts
@@ -1,15 +1,16 @@
 import * as Yup from 'yup'
 
 const constructionSchema = Yup.object().shape({
+  id: Yup.number().required('Construction ID is required'),
+  isDeleting : Yup.boolean().required(),
+  dateCreated: Yup.string().required(),
+  dateUpdated: Yup.string().required(),
   name: Yup.string().required('Name is required'),
   address: Yup.string().required('Address is required'),
   type: Yup.string().required('Type is required'),
 })
 
-type Construction = Yup.InferType<typeof constructionSchema> & {
-  isDeleting : boolean
-  id: number
-}
+interface Construction extends Yup.Asserts<typeof constructionSchema> {}
 
 type Constructions = Array<Construction> | null
 
