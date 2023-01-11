0687858f738b3beabbe831799d43aee2f11f5c52
Elias <elias.ihma.gmail.com>
2023-01-11 10:42:23 +0100

    fix getserversideprops

--- pages/constructions/edit/[id].tsx
+++ pages/constructions/edit/[id].tsx
@@ -1,18 +1,15 @@
 import { AddEdit } from 'components/constructions'
 import { constructionService } from 'services'
-import { GetServerSideProps } from 'next'
+import { GetServerSideProps, GetServerSidePropsResult } from 'next'
 import { Construction } from 'types'
 
 export default AddEdit
 
 export const getServerSideProps:GetServerSideProps<Construction> = async({params}) => {
   const construction = await constructionService.getById(params && params.id) 
+  const result:GetServerSidePropsResult<Construction> = { props : { construction } } 
   // TODO : execité côté serveur, il faut accéder directement à la ressource
   // sinon passer la construction en props et appeler la méthode getById() 
-  
-  return {
-    props: {
-      construction
-    }
-  }
+  // Type 'Promise<{ props: { construction: Construction; }; }>' is not assignable to type 'Promise<GetServerSidePropsResult<Construction>>'.
+  return result 
 }
