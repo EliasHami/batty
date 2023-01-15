8c3662c312b2f803fe07e6ae8865e7b02f51b622
hamie <elias.hami-ext@mfi.fr>
2023-01-15 14:59:30 +0100

    added tabs for constructions

--- app/constructions/AddEdit.tsx
+++ app/constructions/AddEdit.tsx
@@ -6,8 +6,13 @@ import { yupResolver } from '@hookform/resolvers/yup'
 import { constructionService, alertService, Alert } from 'services'
 import { Construction, constructionSchema } from 'types'
 import { getErrorMessage } from 'helpers'
-import { Link } from 'components'
+import { getTabProps, Link, TabPanel } from 'components'
 import Parts from './Parts'
+
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import { useState } from 'react'
 
 type AddEditProps = {
   construction?: Construction | null
@@ -16,6 +21,7 @@ type AddEditProps = {
 const AddEdit: React.FC<AddEditProps> = ({ construction }) => {
   const isAddMode = !construction
   const router = useRouter()
+  const [eventKey, setEventKey] = useState(0)
 
   const formOptions: UseFormProps<Construction> = {
     resolver: yupResolver(constructionSchema),
@@ -56,61 +62,77 @@ const AddEdit: React.FC<AddEditProps> = ({ construction }) => {
     }
   }
 
+  const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
+    setEventKey(newValue)
+  }
+
   return (
     <FormProvider {...methods} >
       <form id="hook-form" onSubmit={handleSubmit(onSubmit)}>
-        <h1>{isAddMode ? 'Add Construction' : 'Edit Construction'}</h1>
-        <div className="form-row">
-          <div className="form-group col">
-            <label>Name</label>
-            <input type="text" {...register("name" as never)} className={'form-control' + (errors.name ? ' is-invalid' : '')} />
-            <div className="invalid-feedback">{errors.name?.message}</div>
+        <Box sx={{ width: '100%' }}>
+          <h1>{isAddMode ? 'Add Construction' : 'Edit Construction'}</h1>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <Tabs value={eventKey} onChange={handleTabChange} aria-label="constructions tabs">
+              <Tab label="Configuration" {...getTabProps(0)} />
+              <Tab label="Parts" {...getTabProps(1)} />
+            </Tabs>
+          </Box>
+          <TabPanel value={eventKey} index={0}>
+            <div className="form-row">
+              <div className="form-group col">
+                <label>Name</label>
+                <input type="text" {...register("name" as never)} className={'form-control' + (errors.name ? ' is-invalid' : '')} />
+                <div className="invalid-feedback">{errors.name?.message}</div>
+              </div>
+              <div className="form-group col">
+                <label>Address</label>
+                <input type="text" {...register("address" as never)} className={'form-control' + (errors.address ? ' is-invalid' : '')} />
+                <div className="invalid-feedback">{errors.address?.message}</div>
+              </div>
+              <div className="form-group col">
+                <label>Description</label>
+                <input type="text" {...register("description" as never)} className={'form-control' + (errors.description ? ' is-invalid' : '')} />
+                <div className="invalid-feedback">{errors.description?.message}</div>
+              </div>
+              <div className="form-group col">
+                <label>Customer</label>
+                <input type="text" {...register("customer" as never)} className={'form-control' + (errors.customer ? ' is-invalid' : '')} />
+                <div className="invalid-feedback">{errors.customer?.message}</div>
+              </div>
+              <div className="form-group col">
+                <label>Address</label>
+                <input type="text" {...register("address" as never)} className={'form-control' + (errors.address ? ' is-invalid' : '')} />
+                <div className="invalid-feedback">{errors.address?.message}</div>
+              </div>
+              <div className="form-group col">
+                <label>Estimate&apos;s validity</label>
+                <input type="text" {...register("estimate_validity" as never)} className={'form-control' + (errors.estimate_validity ? ' is-invalid' : '')} />
+                <div className="invalid-feedback">{errors.estimate_validity?.message}</div>
+              </div>
+            </div>
+          </TabPanel>
+          <TabPanel value={eventKey} index={1}>
+            <div className="form-row">
+              <Parts />
+            </div>
+          </TabPanel>
+          <div className="form-row">
+            <div className="form-group">
+              <button type="submit" disabled={formState.isSubmitting} className="btn btn-primary mr-2">
+                {formState.isSubmitting && <span className="spinner-border spinner-border-sm mr-1"></span>}
+                Save
+              </button>
+              <button
+                onClick={() => reset(formOptions.defaultValues)}
+                type="button"
+                disabled={formState.isSubmitting}
+                className="btn btn-secondary">
+                Reset
+              </button>
+              <Link href="/constructions" className="btn btn-link">Cancel</Link>
+            </div>
           </div>
-          <div className="form-group col">
-            <label>Address</label>
-            <input type="text" {...register("address" as never)} className={'form-control' + (errors.address ? ' is-invalid' : '')} />
-            <div className="invalid-feedback">{errors.address?.message}</div>
-          </div>
-          <div className="form-group col">
-            <label>Description</label>
-            <input type="text" {...register("description" as never)} className={'form-control' + (errors.description ? ' is-invalid' : '')} />
-            <div className="invalid-feedback">{errors.description?.message}</div>
-          </div>
-          <div className="form-group col">
-            <label>Customer</label>
-            <input type="text" {...register("customer" as never)} className={'form-control' + (errors.customer ? ' is-invalid' : '')} />
-            <div className="invalid-feedback">{errors.customer?.message}</div>
-          </div>
-          <div className="form-group col">
-            <label>Address</label>
-            <input type="text" {...register("address" as never)} className={'form-control' + (errors.address ? ' is-invalid' : '')} />
-            <div className="invalid-feedback">{errors.address?.message}</div>
-          </div>
-          <div className="form-group col">
-            <label>Estimate&apos;s validity</label>
-            <input type="text" {...register("estimate_validity" as never)} className={'form-control' + (errors.estimate_validity ? ' is-invalid' : '')} />
-            <div className="invalid-feedback">{errors.estimate_validity?.message}</div>
-          </div>
-        </div>
-        <div className="form-row">
-          <Parts />
-        </div>
-        <div className="form-row">
-          <div className="form-group">
-            <button type="submit" disabled={formState.isSubmitting} className="btn btn-primary mr-2">
-              {formState.isSubmitting && <span className="spinner-border spinner-border-sm mr-1"></span>}
-              Save
-            </button>
-            <button
-              onClick={() => reset(formOptions.defaultValues)}
-              type="button"
-              disabled={formState.isSubmitting}
-              className="btn btn-secondary">
-              Reset
-            </button>
-            <Link href="/constructions" className="btn btn-link">Cancel</Link>
-          </div>
-        </div>
+        </Box>
       </form>
     </FormProvider>
   )
--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,7 @@ import NavLink from './NavLink'
 import Nav from './Nav'
 import Link from './Link'
 import Alert from './Alert'
+import TabPanel from './TabPanel'
+import { getTabProps } from './TabPanel'
 
-export { Nav, NavLink, Link, Alert,  }
+export { Nav, NavLink, Link, Alert, TabPanel, getTabProps }
