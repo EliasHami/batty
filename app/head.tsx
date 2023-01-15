ccf10ec10d83038a22c402e10ccdd07dab774a63
hamie <elias.hami-ext@mfi.fr>
2023-01-15 19:45:11 +0100

    normalize css

--- app/head.tsx
+++ app/head.tsx
@@ -2,6 +2,7 @@ export default function Head() {
   return (
     <>
        <title>Batty</title>
+       <meta name="viewport" content="initial-scale=1, width=device-width" />
     </>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 import './globals.css'
 import { Nav, Alert } from 'components'
+import CssBaseline from '@mui/material/CssBaseline'
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <body>
         <div className="app-container bg-light">
+          <CssBaseline />
           <Nav />
           <Alert />
           <div className='container pt-4 bp-4'>
