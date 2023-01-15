150ddc84d21e1dae46526d33968be4064b546b6c
hamie <elias.hami-ext@mfi.fr>
2023-01-15 20:32:21 +0100

    added stepper to addedit

--- app/constructions/AddEdit.tsx
+++ app/constructions/AddEdit.tsx
@@ -6,22 +6,22 @@ import { yupResolver } from '@hookform/resolvers/yup'
 import { constructionService, alertService, Alert } from 'services'
 import { Construction, constructionSchema } from 'types'
 import { getErrorMessage } from 'helpers'
-import { getTabProps, Link, TabPanel } from 'components'
+import { Link } from 'components'
 import Parts from './Parts'
 
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Box from '@mui/material/Box';
 import { useState } from 'react'
+import { Step, StepLabel, Stepper,Box, Button } from '@mui/material'
 
 type AddEditProps = {
   construction?: Construction | null
 }
 
+const steps = ["Configuration", "Select Parts"]
+
 const AddEdit: React.FC<AddEditProps> = ({ construction }) => {
   const isAddMode = !construction
   const router = useRouter()
-  const [eventKey, setEventKey] = useState(0)
+  const [activeStep, setActiveStep] = useState(0)
 
   const formOptions: UseFormProps<Construction> = {
     resolver: yupResolver(constructionSchema),
@@ -46,7 +46,7 @@ const AddEdit: React.FC<AddEditProps> = ({ construction }) => {
     try {
       await constructionService.create(data)
       alertService.success('Construction added successfully', { keepAfterRouteChange: true } as Alert)
-      router.push('.')
+      router.push('./constructions')
     } catch (error) {
       alertService.error(getErrorMessage(error))
     }
@@ -56,14 +56,18 @@ const AddEdit: React.FC<AddEditProps> = ({ construction }) => {
     try {
       await constructionService.update(id, data)
       alertService.success('Construction updated successfully', { keepAfterRouteChange: true } as Alert)
-      router.push('.')
+      router.push('./constructions')
     } catch (error) {
       alertService.error(getErrorMessage(error))
     }
   }
 
-  const handleTabChange = (event: React.SyntheticEvent, newValue: number) => {
-    setEventKey(newValue)
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1)
+  }
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1)
   }
 
   return (
@@ -71,13 +75,10 @@ const AddEdit: React.FC<AddEditProps> = ({ construction }) => {
       <form id="hook-form" onSubmit={handleSubmit(onSubmit)}>
         <Box sx={{ width: '100%' }}>
           <h1>{isAddMode ? 'Add Construction' : 'Edit Construction'}</h1>
-          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-            <Tabs value={eventKey} onChange={handleTabChange} aria-label="constructions tabs">
-              <Tab label="Configuration" {...getTabProps(0)} />
-              <Tab label="Parts" {...getTabProps(1)} />
-            </Tabs>
-          </Box>
-          <TabPanel value={eventKey} index={0}>
+          <Stepper activeStep={activeStep}>
+            {steps.map(label => <Step key={label}><StepLabel>{label}</StepLabel></Step>)}
+          </Stepper>
+          {steps[activeStep] === "Configuration" && (
             <div className="form-row">
               <div className="form-group col">
                 <label>Name</label>
@@ -109,26 +110,42 @@ const AddEdit: React.FC<AddEditProps> = ({ construction }) => {
                 <input type="text" {...register("estimate_validity" as never)} className={'form-control' + (errors.estimate_validity ? ' is-invalid' : '')} />
                 <div className="invalid-feedback">{errors.estimate_validity?.message}</div>
               </div>
-            </div>
-          </TabPanel>
-          <TabPanel value={eventKey} index={1}>
+            </div>)}
+          {steps[activeStep] === "Select Parts" && (
             <div className="form-row">
               <Parts />
-            </div>
-          </TabPanel>
+            </div>)}
           <div className="form-row">
             <div className="form-group">
-              <button type="submit" disabled={formState.isSubmitting} className="btn btn-primary mr-2">
-                {formState.isSubmitting && <span className="spinner-border spinner-border-sm mr-1"></span>}
-                Save
-              </button>
-              <button
-                onClick={() => reset(formOptions.defaultValues)}
-                type="button"
-                disabled={formState.isSubmitting}
-                className="btn btn-secondary">
-                Reset
-              </button>
+              {activeStep === steps.length ? (
+                <>
+                  <button type="submit" disabled={formState.isSubmitting} className="btn btn-primary mr-2">
+                    {formState.isSubmitting && <span className="spinner-border spinner-border-sm mr-1"></span>}
+                    Save
+                  </button>
+                  <button
+                    onClick={() => reset(formOptions.defaultValues)}
+                    type="button"
+                    disabled={formState.isSubmitting}
+                    className="btn btn-secondary">
+                    Reset
+                  </button>
+                </>) : (
+                <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                  <Button
+                    color="inherit"
+                    disabled={activeStep === 0}
+                    onClick={handleBack}
+                    sx={{ mr: 1 }}
+                  >
+                    Back
+                  </Button>
+                  <Box sx={{ flex: '1 1 auto' }} />
+                  <Button onClick={handleNext}>
+                     Next
+                  </Button>
+                </Box>
+              )}
               <Link href="/constructions" className="btn btn-link">Cancel</Link>
             </div>
           </div>
