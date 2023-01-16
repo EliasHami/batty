2da8c9b19bfbc5571c66286bffb6b26a8b45f966
hamie <elias.hami-ext@mfi.fr>
2023-01-16 15:27:00 +0100

    added accordion to parts

--- app/constructions/Parts.tsx
+++ app/constructions/Parts.tsx
@@ -1,35 +1,65 @@
 "use client"
 import { useFormContext, useFieldArray } from "react-hook-form"
+import * as React from 'react'
 import Provisions from "./Provisions"
+import Accordion from '@mui/material/Accordion'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import AccordionDetails from '@mui/material/AccordionDetails'
+import Typography from '@mui/material/Typography'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import { Box } from "@mui/system"
 
 export default function Parts() {
-  const { control, register, formState } = useFormContext()
+  const { control, register, formState, watch } = useFormContext()
   const { errors } = formState
   const { fields, append, remove } = useFieldArray({
     control,
     name: "parts"
-  });
+  })
+
+  const [expanded, setExpanded] = React.useState<string | false>(false)
+
+  const handleExpand =
+    (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+      setExpanded(isExpanded ? panel : false)
+    }
+
+  const getName = (index:number) => watch(`parts.${index}.name`)
 
   return (
-    <>
+    <Box sx={{ display: "flex", flexDirection: "column", justifyContent:"space-between", height:"100%" }}>
       {fields.map((item, index) => (
-        <div key={item.id}>
-          <div className="form-group col">
-            <label>Part name</label>
-            <input type="text" {...register(`parts.${index}.name`)} className={'form-control' + (errors.parts?.[index] ? ' is-invalid' : '')} />
-            <div className="invalid-feedback">{errors.parts?.[index].name}</div>
-          </div>
-          <Provisions />
-          <button type="button" className="btn btn-danger" onClick={() => remove(index)}>Delete</button>
-        </div>
+        <Accordion expanded={expanded === `panel${index}`} onChange={handleExpand(`panel${index}`)} key={item.id} >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <Box sx={{ width :"100%", display: "flex", justifyContent : "space-between"}}>
+              <Typography>{ getName(index) || `Part ${index}`}</Typography>
+              <button type="button" className="btn btn-danger" onClick={() => remove(index)}>X</button>
+            </Box>
+          </AccordionSummary>
+          <AccordionDetails>
+            <div className="form-group col">
+              <label>Part name</label>
+              <input type="text" {...register(`parts.${index}.name`)} className={'form-control' + (errors.parts?.[index] ? ' is-invalid' : '')} />
+              <div className="invalid-feedback">{errors.parts?.[index]?.name}</div>
+            </div>
+            <Provisions />
+          </AccordionDetails>
+        </Accordion>
       ))}
       <button
         type="button"
         className="btn btn-secondary"
-        onClick={() => append({ name:"", provisions : [] })}
+        onClick={() => {
+          append({ name: "", provisions: [] })
+          setExpanded(`panel${fields.length}`)
+        }}
       >
         Add
       </button>
-    </>
+    </Box>
   )
 }
--- app/constructions/Provisions.tsx
+++ app/constructions/Provisions.tsx
@@ -1,7 +1,8 @@
 "use client"
+import { Box, Typography } from "@mui/material";
 import { useFormContext, useFieldArray } from "react-hook-form"
 
-export default function Provisions({}){
+export default function Provisions({ }) {
   const { control, register, formState } = useFormContext()
   const { errors } = formState
   const { fields, append, remove } = useFieldArray({
@@ -10,9 +11,10 @@ export default function Provisions({}){
   });
 
   return (
-    <>
+    <Box sx={{ display: "flex", flexDirection: "column" }}>
+      <Typography>Provisions</Typography>
       {fields.map((item, index) => (
-        <div key={item.id}>
+        <Box sx={{ width: "100%", display: "flex", justifyContent: "space-between" }} key={item.id}>
           <div className="form-group col">
             <label>Provsion name</label>
             <input type="text" {...register(`provisions.${index}.name`)} className={'form-control' + (errors.provisions?.[index] ? ' is-invalid' : '')} />
@@ -23,16 +25,16 @@ export default function Provisions({}){
             <input type="text" {...register(`provisions.${index}.service`)} className={'form-control' + (errors.provisions?.[index] ? ' is-invalid' : '')} />
             <div className="invalid-feedback">{errors.provisions?.[index].service}</div>
           </div>
-          <button type="button" className="btn btn-danger" onClick={() => remove(index)}>Delete</button>
-        </div>
+          <button type="button" className="btn btn-danger" onClick={() => remove(index)}>X</button>
+        </Box>
       ))}
       <button
         type="button"
         className="btn btn-secondary"
-        onClick={() => append({ quantity:"", service : "" })}
+        onClick={() => append({ quantity: "", service: "" })}
       >
         Add
       </button>
-    </>
+    </Box>
   )
 }
