ac200e50dd9040566b3ec352b6fb2e0f06f03666
hamie <elias.hami-ext@mfi.fr>
2023-01-31 11:35:07 +0100

    fix delete

--- app/constructions/Delete.tsx
+++ app/constructions/Delete.tsx
@@ -5,30 +5,31 @@ import { deleteConstruction } from 'src/graphql/mutations'
 import { alertService } from 'services'
 import { getErrorMessage } from 'helpers'
 
-export default function Delete({id}:{id:string}) {
+export default function Delete({ id }: { id: string }) {
   const [isDeleting, setIsDeleting] = useState(false)
   async function handleDelete(id: string) {
-    setIsDeleting(true)    
+    setIsDeleting(true)
     try {
-      await API.graphql({
+      API.graphql({
         authMode: 'AMAZON_COGNITO_USER_POOLS',
         query: deleteConstruction,
         variables: { input: { id } }
       })
     } catch (error) {
       alertService.error(getErrorMessage(error))
-    }    
+    }
+    setIsDeleting(false)
   }
 
   return (
-    <button 
-      className="btn btn-sm btn-danger btn-construction" 
-      onClick={() => handleDelete(id)} 
+    <button
+      className="btn btn-sm btn-danger btn-construction"
+      onClick={() => handleDelete(id)}
       disabled={isDeleting}
     >
-      {isDeleting 
-          ? <span className="spinner-border spinner-border-sm"></span> 
-          : <span>Delete</span>}
+      {isDeleting
+        ? <span className="spinner-border spinner-border-sm"></span>
+        : <span>Delete</span>}
     </button>
   )
 }
